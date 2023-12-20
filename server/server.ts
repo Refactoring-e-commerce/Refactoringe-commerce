9ac674f978ac9f071c7f07ff95f33578d49a3d20
nessrineatig <nesrineatig14@gmail.com>
2023-12-20 14:43:45 +0100

    wallet & favorite back done(still update only)

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
+import walletRout from './routes/wallet_route'
+import favoriteRout from './routes/favorite_route'
 
 const app = express();
 const PORT = 8080;
 app.use(cors());
 app.use(express.json());
+app.use("/wallet",walletRout)
+app.use("/favorite",favoriteRout)
 
 app.listen(PORT, () => {
   console.log(`server listening on http://localhost:${PORT}`);
