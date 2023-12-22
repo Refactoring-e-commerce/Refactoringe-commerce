5238bde5890fa44b07f079b7ca94bf4c5af237f5
widouu <wided.fathallah03@gmail.com>
2023-12-22 13:37:17 +0100

    pulling time

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,12 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { NextAuthProvider } from "./providers";
+import Navbar from "./Components/navbar";
+import Footer from "./Components/footer"
+
+
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,15 +18,18 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + " flex flex-row"}>
-        <NextAuthProvider>
-          {children}
-        </NextAuthProvider>
+      <body className={inter.className + "flex"}>
+        <Navbar />
+        <NextAuthProvider>{children}</NextAuthProvider>
+        <Footer/>
       </body>
     </html>
   );
 }
+
+
+
