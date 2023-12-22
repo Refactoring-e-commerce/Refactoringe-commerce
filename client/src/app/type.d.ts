498b3f1c8d88f2e2bcc9499581623ea1c5bc5818
nessrineatig <nesrineatig14@gmail.com>
2023-12-22 13:22:52 +0100

    fav list

--- client/src/app/type.d.ts
+++ client/src/app/type.d.ts
@@ -1,11 +1,21 @@
 interface Brand {
-  id: String;    
+  id: String;
   fullName:   String;
-  email:      String ;   
+  email:      String ;
   password :  String;
   image:     String;
   cover:      String;
   bio:       String
 }
-
+interface Product {
+  id: String;
+  name :       String;
+  image :    String[];
+  description : String;
+  price     :   Int;
+  category   :  String;
+  quantity   :  Int;
+  reference  :  String;
+  status     :  Boolean;
+}
 
--- client/src/app/utils/useApi.ts
+++ client/src/app/utils/useApi.ts
@@ -1,4 +1,17 @@
+
 import { useQuery, useMutation } from "@tanstack/react-query";
+export const getAllproduct = () => {
+  const query = useQuery<Product[]>({
+    queryKey: ["product"],
+    queryFn: async () => {
+      const result = await fetch("http://localhost:8080/Product/product");
+      const data = await result.json();
+      return data;
+    },
+    select: (data) => data,
+  });
+  return query;
+};
 
 export const getoneBrandProfile = () => {
   const query = useQuery<Brand[]>({
